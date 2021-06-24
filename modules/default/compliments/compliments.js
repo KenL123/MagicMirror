f441115decff9b22d8ff028b0bbad901cc2cc6d1
Ken Liu <kenliu380@gmail.com>
2021-06-24 12:35:32 -0700

    Worked on some modules

--- modules/default/compliments/compliments.js
+++ modules/default/compliments/compliments.js
@@ -8,10 +8,10 @@ Module.register("compliments", {
 	// Module config defaults.
 	defaults: {
 		compliments: {
-			anytime: ["Hey there sexy!"],
-			morning: ["Good morning, handsome!", "Enjoy your day!", "How was your sleep?"],
-			afternoon: ["Hello, beauty!", "You look sexy!", "Looking good today!"],
-			evening: ["Wow, you look hot!", "You look nice!", "Hi, sexy!"],
+			anytime: ["Hi Ken! How's it going?"],
+			morning: ["Good Morning Ken!"],
+			afternoon: ["Good Afternoon Ken!"],
+			evening: ["Good Nigh Ken!"],
 			"....-01-01": ["Happy new year!"]
 		},
 		updateInterval: 30000,
